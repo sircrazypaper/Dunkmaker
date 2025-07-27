ce301a2e3b187baacc5e5b4ad316fdd164b0b771
sircrazypaper <127138479+sircrazypaper@users.noreply.github.com>
2025-07-27 18:48:28 -0400

    Add files via upload

---
+++ dunkmaker.js
@@ -0,0 +1,18 @@
+let finishes = ['One-handed', 'Two-handed', 'Scorpion'];
+let finish = '';
+let spins = ['', '180', '360', '540', '720'];
+let spin = '';
+let transfers = ['Between the legs', 'Behind the back', 'Under both', 'J-Rich'];
+let transfer ='';
+let dunk = '';
+
+func createDunk(){
+  spin = spins[Math.floor(Math.random()*spins.length)];
+  transfer = transfers[Math.floor(Math.random()*transfers.length)];
+  finish = finishes[Math.floor(Math.random()*finishes.length)];
+  dunk = spin+" "+transfer+" "+finish;
+
+  console.log(dunk);
+}
+
+createDunk();
