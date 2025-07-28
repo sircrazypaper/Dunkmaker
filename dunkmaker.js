7117d9d87a873f1775477b376f7edca2a95bec49
sircrazypaper <127138479+sircrazypaper@users.noreply.github.com>
2025-07-28 19:37:36 -0400

    Adding more dunk combo logic

--- dunkmaker.js
+++ dunkmaker.js
@@ -16,9 +16,15 @@ function createDunk(){
 };
 
 function createDunkList(){
-  dunks.push(finishes);
-  //For every spin add every finish
+  dunks = [];
+
+  for (let i of spins){
+    for (let j of finishes){
+      dunk = i+" "+j;
+      dunks.push(dunk);
+    };
+  };
   //for every transfer add every finish
   //for every spin add every transfer and every finish
-  document.getElementById('dunkListDisplay').innerHTML = dunks;
+  document.getElementById('dunkListDisplay').innerHTML = dunks.join("<br>");
 };
